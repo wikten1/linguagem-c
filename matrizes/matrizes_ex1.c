3907a1026655dca4d756bb81e2d0c1282c7c6fdf
Wikten Alves <wiktenalves@gmail.com>
2023-07-06 21:51:45 -0300

    Exercícios de matrizes feitos

---
+++ matrizes/matrizes_ex1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+    //var
+    int mat [3] [3];
+
+    //ini
+    mat [0] [0] = 1;
+    mat [0] [1] = 2;
+    mat [0] [2] = 3;
+
+    mat [1] [0] = 4;
+    mat [1] [1] = 5;
+    mat [1] [2] = 6;
+
+    mat [2] [0] = 7;
+    mat [2] [1] = 8;
+    mat [2] [2] = 9;
+
+    printf("Imprimindo a primeira linha:\n");
+    printf("%d %d %d\n", mat[0][0], mat[0][1], mat[0][2]);
+
+    return 0;
+}
---
+++ matrizes/matrizes_ex2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    //var
+    int mat[3][3]={{1,2,3},
+                   {4,5,6},
+                   {7,8,9}};
+
+    int i, j;
+    //ini
+    printf("Imprimindo a primeira linha:\n");
+    for (j = 0; j < 3; j++)
+    {
+        printf("%d", mat[0][j]);
+    }
+
+    printf("\n\nImprimindo a matriz toda:\n");
+    for ( i = 0; i < 3; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            printf("%d", mat[i][j]);
+        }
+        
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
