ce418723bc4d56b24e1c762118c1cf0760c3ce21
Wikten Alves <wiktenalves@gmail.com>
2023-07-08 22:30:16 -0300

    ex1 de função feito

---
+++ funcoes/funcao_ex1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+float maior (float Num1, float Num2) {
+    if (Num1 > Num2)
+    {
+        return Num1;
+    } else{
+        return Num2;
+    }
+}
+
+int main (){
+    //var
+    float x, y, m;
+
+    //ini
+    printf("Insira um valor:\n");
+    scanf("%f", &x);
+    printf("Insira mais um valor:\n");
+    scanf("%f", &y);
+
+    m = maior(x,y);
+
+    printf("Maior: %.2f\n", m);
+
+    return 0;
+}
